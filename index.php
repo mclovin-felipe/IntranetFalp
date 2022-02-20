740c3f35ce2789a182646584d04a161f6a9f1944
mclovin-felipe <felipe.ponce2@mail.udp.cl>
2022-02-20 17:41:54 -0300

    Arreglo scroll Y

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css">
  	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://use.typekit.net/qqw3nvm.css">
@@ -21,13 +21,12 @@
 <!----->
 <!----->
 <!----->
-
-<div class="d-flex">
-	<div id="sidebar-container" class = "bg-primary">
-		<div class="logo">
-			<h4 class="text-light" style="font-weight: bold;">Logs</h4>
-		</div>
-		<div class = "menu">
+<div class="wrapper"> 
+	<div id="sidebar" class = "bg-primary">
+<div class="sidebar-header text-light">
+                <h3>Bootstrap Sidebar</h3>
+            </div>
+		<div class = "active">
 <!--			<a href= "#" class="d-block text-light p-3"><i class="icon ion-md-apps mr-2 lead"></i> Tablero</a>
 			<a href= "#" class="d-block text-light p-3"><i class="icon ion-md-stats mr-2 lead"></i> Filtros</a>
 		    <a href= "#" class="d-block text-light p-3"><i class="icon ion-md-person mr-2 lead"></i> Perfil</a>
@@ -44,7 +43,7 @@
 	   value="00:00"
        min="00:00:00" max="23:59:59">
 <input type="date" id="fecha2" name="fecha2"
-value=<?php echo date('Y-m-d');?>
+value=<?php echo date("Y-m-d"); ?>
        min="2001-01-01" max="2025-12-31">
 <input class ="m-2"type="time" id="hora2" name="hora2"
 	   value="23:59"
@@ -56,7 +55,7 @@ value=<?php echo date('Y-m-d');?>
 </form>
 <div class="mb-3 p-3">
       <label for="disabledSelect" class="form-label">Disabled select menu</label>
-      <select id="disabledSelect" class="form-select">
+      <select id="Metodos" class="form-select">
         <option>Disabled select</option>
       </select>
     </div>
@@ -67,11 +66,20 @@ value=<?php echo date('Y-m-d');?>
 <!--TABLA-->
 <!----->
 <!----->
-<div class="container">
+<div class="content">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+
+            <button type="button" id="sidebarCollapse" class="btn btn-info">
+                <i class="fas fa-align-left"></i>
+                <span>Toggle Sidebar</span>
+            </button>
+
+        </div>
+    </nav>
 <h2 class = "center">Tablas jeje</h2>
-<div id="table-container"></div>
-<table id="table_id" class="display">
-    <thead>
+<table id="table_id" class="table table-striped">
+<thead>
         <tr>
             <th>Log Origen</th>
             <th>Fecha</th>
@@ -87,13 +95,16 @@ value=<?php echo date('Y-m-d');?>
 	</tbody>
 </table>
 </div>
+</div>
  </div>
 <!----->
 <!----->
 <!----->
 <!----->
+</div>
  </body>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
 <script src="js/scripts.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,63 @@
- //$(document).ready(function(){
- //     $.ajax({
- //       method: "GET",
+//$(document).ready(function(){
+//     $.ajax({
+//       method: "GET",
 //
-  //      url: "data.php?fecha1=20210210&fecha2=20211212",
-    //  }).done(function( data ) {
-      //            var result= $.parseJSON(data);
-        //          let tabla = $('#table_id').DataTable();
-          //        tabla.clear();
-            //     $.each( result, function( key, value ) {
-              //          tabla.row.add([
-                //                value['log_origen'], value['fecha'], value['metodo'], value['enlace'], value['estado'], value['tiempo_respuesta'], value['IP_cliente'], value['largo_peticion']
-                  //       ]).draw();
-             // });
-         // });
-        //});/
-$("#update").click(function(){
-		let fecha1 = $("#fecha1").val().replace(/-/g, "");
-		let fecha2 = $("#fecha2").val().replace(/-/g, "");
-		let hora1 = $("#hora1").val()
-		let hora2 = $("#hora1").val()
-		console.log($("#hora1").val());
-	
-      $.ajax({
-        method: "GET",
-		  url: `data.php?fecha1=${fecha1}&hora1=${hora1}&fecha2=${fecha2}&hora2=${hora2}`,
-      }).done(function( data ) {
-                  var result= $.parseJSON(data);
-		  let tabla = $('#table_id').DataTable({
-					screenY: '40vh',
-			  		scrollCollapse: true,
-			  paging:         false,
-			  searching: false,
-			  retrieve: true
-		  });
-                  tabla.clear();
-                 $.each( result, function( key, value ) {
-                        tabla.row.add([
-                                value['log_origen'], value['fecha'], value['metodo'], value['enlace'], value['estado'], value['tiempo_respuesta'], value['IP_cliente'], value['largo_peticion']
-                         ]).draw();
-              });
-          });
+//      url: "data.php?fecha1=20210210&fecha2=20211212",
+//  }).done(function( data ) {
+//            var result= $.parseJSON(data);
+//          let tabla = $('#table_id').DataTable();
+//        tabla.clear();
+//     $.each( result, function( key, value ) {
+//          tabla.row.add([
+//                value['log_origen'], value['fecha'], value['metodo'], value['enlace'], value['estado'], value['tiempo_respuesta'], value['IP_cliente'], value['largo_peticion']
+//       ]).draw();
+// });
+// });
+//});/
+$("#update").click(function () {
+	let fecha1 = $("#fecha1").val().replace(/-/g, "");
+	let fecha2 = $("#fecha2").val().replace(/-/g, "");
+	let hora1 = $("#hora1").val();
+	let hora2 = $("#hora1").val();
+
+	$.ajax({
+		method: "GET",
+		url: `data.php?fecha1=${fecha1}&hora1=${hora1}&fecha2=${fecha2}&hora2=${hora2}`,
+	}).done(function (data) {
+		var result = $.parseJSON(data);
+		let tabla = $("#table_id").DataTable({
+			scrollCollapse: true,
+			searching: false,
+			retrieve: true,
+		});
+		let metodos = [];
+		tabla.clear();
+		$.each(result, function (key, value) {
+
+			if (!metodos.includes(value['metodo'])){
+				console.log(value['metodo']);
+				$('#metodos').append(`<option>${value['metodo']}</option>`);
+			}
+			tabla.row
+				.add([
+					value["log_origen"],
+					value["fecha"],
+					value["metodo"],
+					value["enlace"],
+					value["estado"],
+					value["tiempo_respuesta"],
+					value["IP_cliente"],
+					value["largo_peticion"],
+				])
+				.draw();
+		});
+	});
+});
+$(document).ready(function () {
+	$("#sidebarCollapse").on("click", function () {
+		$("#sidebar").toggleClass("active");
+	});
+});
+$(window).resize(function() {
+  $('.dataTables_scrollBody').css('height', ($(window).height() - 200));
 });
